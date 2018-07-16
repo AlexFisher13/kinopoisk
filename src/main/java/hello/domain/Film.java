4b977b114686b8e181629780bec2aa0d8a7f51c5
Tkachenko AI <alexfisher13@yandex.ru>
2018-07-16 15:37:59 +0300

    добавил коммент

--- src/main/java/hello/domain/Film.java
+++ src/main/java/hello/domain/Film.java
@@ -17,41 +17,32 @@ public class Film {
 
     public Film() {
     }
-
     public Film(String name, String rating, String year) {
         this.name = name;
         this.rating = rating;
         this.year = year;
     }
-
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getRating() {
         return rating;
     }
-
     public void setRating(String rating) {
         this.rating = rating;
     }
-
     public String getYear() {
         return year;
     }
-
     public void setYear(String year) {
         this.year = year;
     }
